baf3280f319b115e227033dc38b125572d2c040b
Abhijit Hanumeswara Kulkarni <hanumesw@usc.edu>
2026-07-24 14:49:33 -0500

    Time: 0 ms (100%), Space: 20.4 MB (58.12%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,19 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # Brute force - Time = O(n^2), space = O(1)
-        n = len(nums)
-        for i in range(n-1):
-            for j in range(i+1, n):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
-        return [-1, -1] # Never reached according to desc
+        # # Brute force - Time = O(n^2), space = O(1)
+        # n = len(nums)
+        # for i in range(n-1):
+        #     for j in range(i+1, n):
+        #         if nums[i] + nums[j] == target:
+        #             return [i, j]
+        # return [-1, -1] # Never reached according to desc
+
+        # Hmap
+        hmap = {}
+        for index, num in enumerate(nums):
+            if target-num in hmap:
+                return [index, hmap[target-num]]
+            else:
+                hmap[num] = index
+        return [-1, -1] # Never reached
+        
