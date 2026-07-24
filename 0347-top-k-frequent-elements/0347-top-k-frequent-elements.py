1598e69753716cafeeca294217d90a7903ce48b9
Abhijit Hanumeswara Kulkarni <hanumesw@usc.edu>
2026-07-24 17:04:53 -0500

    Time: 0 ms (100%), Space: 22.8 MB (80.24%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,7 +1,22 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # Count freq + Sort
+        # # Count freq + Sort - Time = O(n*log(n)), space = O(n)
+        # from collections import Counter
+        # freq = Counter(nums)
+        # freq = dict(sorted(freq.items(), key=lambda x: -x[1]))
+        # return list(freq.keys())[:k]
+
+        # Follow up - could use heap
+        # heappush = O(log(n)), heappop = O(1), heapify = O(log(n))
         from collections import Counter
+        import heapq
         freq = Counter(nums)
-        freq = dict(sorted(freq.items(), key=lambda x: -x[1]))
-        return list(freq.keys())[:k]
+        heap = []
+        for key, value in freq.items():
+            heapq.heappush(heap, (-value, key))
+        
+        res = []
+        while k > 0:
+            res.append(heapq.heappop(heap)[1])
+            k -= 1
+        return res
