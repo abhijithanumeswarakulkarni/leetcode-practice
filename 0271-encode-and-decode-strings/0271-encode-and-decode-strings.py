dfc772e99e226e2e332b377899d9261846d97eec
Abhijit Hanumeswara Kulkarni <hanumesw@usc.edu>
2026-07-24 01:09:30 -0500

    Time: 57 ms (98.55%), Space: 19.5 MB (85.55%) - LeetHub

---
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
@@ -0,0 +1,15 @@
+class Codec:
+    def encode(self, strs: List[str]) -> str:
+        return "<->".join(strs)
+        
+
+        
+
+    def decode(self, s: str) -> List[str]:
+        return s.split("<->")
+        
+
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.decode(codec.encode(strs))
