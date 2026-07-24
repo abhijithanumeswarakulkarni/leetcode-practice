1615757d5117fa9d9a6248b40792b1b599c56ceb
Abhijit Hanumeswara Kulkarni <hanumesw@usc.edu>
2026-07-23 22:34:40 -0500

    Time: 23 ms (6.12%), Space: 20.2 MB (18.15%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,18 +1,23 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # Freq map - Time = O(n), space = O(n)
-        from collections import Counter
+        # # Brute Force - Time = O(n), space = O(n)
+        # from collections import Counter
         
-        # Edge case - If lengths don't match
-        if len(s) != len(t):
-            return False
+        # # Edge case - If lengths don't match
+        # if len(s) != len(t):
+        #     return False
 
-        s_freq = Counter(s)
-        t_freq = Counter(t)
-        for char in s:
-            if s_freq[char] == t_freq[char]:
-                del s_freq[char]
-                del t_freq[char]
-            else:
-                return False
-        return True if (len(s_freq) == 0 and len(t_freq) == 0) else False
+        # s_freq = Counter(s)
+        # t_freq = Counter(t)
+        # for char in s:
+        #     if s_freq[char] == t_freq[char]:
+        #         del s_freq[char]
+        #         del t_freq[char]
+        #     else:
+        #         return False
+        # return True if (len(s_freq) == 0 and len(t_freq) == 0) else False
+
+        # Sorting - Time = O(n*log(n)), space = O(1)
+        sorted_s = str(sorted(s))
+        sorted_t = str(sorted(t))
+        return sorted_s == sorted_t
