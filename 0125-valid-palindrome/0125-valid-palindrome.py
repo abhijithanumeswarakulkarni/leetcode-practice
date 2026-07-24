5daafa089f1bf55822d3432cd7c48b555621efeb
Abhijit Hanumeswara Kulkarni <hanumesw@usc.edu>
2026-07-24 17:33:14 -0500

    Time: 7 ms (81.17%), Space: 19.5 MB (93.93%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,8 +1,25 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        # Reverse
-        formatted_s = ''
-        for x in s:
-            if x.isalnum():
-                formatted_s += x.lower()
-        return formatted_s == formatted_s[::-1]
+        # # Brute Force - Time = O(n), space = O(n)
+        # formatted_s = ''
+        # for x in s:
+        #     if x.isalnum():
+        #         formatted_s += x.lower()
+        # return formatted_s == formatted_s[::-1]
+
+        # Two pointer - Time = O(n), space = O(1)
+        i = 0
+        j = len(s) - 1
+        while i < j:
+            if not s[i].isalnum():
+                i += 1
+                continue
+            if not s[j].isalnum():
+                j -= 1
+                continue
+            if s[i].lower() != s[j].lower():
+                return False
+            i += 1
+            j -= 1
+        return True
+            
