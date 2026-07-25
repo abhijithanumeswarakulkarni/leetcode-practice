dc89678245715450c36f253fdb6e737051bb0cec
Abhijit Hanumeswara Kulkarni <hanumesw@usc.edu>
2026-07-24 23:14:42 -0500

    Time: 3718 ms (5.27%), Space: 19.3 MB (27.18%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.py
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.py
@@ -2,15 +2,15 @@ class Solution:
     def countVowelStrings(self, n: int) -> int:
         # Recursion
         vowels = ['a', 'e', 'i', 'o', 'u']
-        def solve(k, temp):
+        def solve(k, prev):
             if k == 0:
                 return 1
             total = 0
-            for v in vowels:
-                if not temp:
-                    total += solve(k-1, v)
-                elif v >= temp[-1]:
-                    total += solve(k-1, temp+v)
+            for vowel in vowels:
+                if not prev:
+                    total += solve(k-1, vowel)
+                elif vowel >= prev:
+                    total += solve(k-1, vowel)
             return total
         
         return solve(n, '')
