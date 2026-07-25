cd57c257948c851d33ab625ca3290a505f3c2bb8
Abhijit Hanumeswara Kulkarni <hanumesw@usc.edu>
2026-07-24 23:53:28 -0500

    Time: 0 ms (100%), Space: 19.2 MB (87.22%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.py
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.py
@@ -15,19 +15,32 @@ class Solution:
         
         # return solve(n, '')
 
-        # DP
-        vowels_priority = [1, 2, 3, 4, 5]
-        def solve(k, prev_priority, dp):
-            if k == 0:
-                return 1
+        # # DP
+        # vowels_priority = [1, 2, 3, 4, 5]
+        # def solve(k, prev_priority, dp):
+        #     if k == 0:
+        #         return 1
             
-            if dp[k][prev_priority] == -1:
+        #     if dp[k][prev_priority] == -1:
+        #         total_ways = 0
+        #         for vowel_prio in vowels_priority:
+        #             if vowel_prio >= prev_priority:
+        #                 total_ways += solve(k-1, vowel_prio, dp)
+        #         dp[k][prev_priority] = total_ways
+        #     return dp[k][prev_priority]
+        
+        # dp = [[-1] * 6 for _ in range(n+1)]
+        # return solve(n, 0, dp)
+
+        # DP + Space optimization
+        dp = [[-1] * 6 for _ in range(n+1)]
+        dp[0] = [1] * 6
+        vowels_priority = [1, 2, 3, 4, 5]
+        for k in range(1, n+1):
+            for prev_priority in range(0, 6):
                 total_ways = 0
                 for vowel_prio in vowels_priority:
                     if vowel_prio >= prev_priority:
-                        total_ways += solve(k-1, vowel_prio, dp)
+                        total_ways += dp[k-1][vowel_prio]
                 dp[k][prev_priority] = total_ways
-            return dp[k][prev_priority]
-        
-        dp = [[-1] * 6 for _ in range(n+1)]
-        return solve(n, 0, dp)
+        return dp[n][0]
