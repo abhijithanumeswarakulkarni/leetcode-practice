f3007345d9ab301059629b2f30ced38af1e7c217
Abhijit Hanumeswara Kulkarni <hanumesw@usc.edu>
2026-07-25 19:51:47 -0500

    Time: 0 ms (100%), Space: 19.4 MB (96.78%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,9 +1,35 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        # Brute Force
-        m, n = len(matrix), len(matrix[0])
+        # # Brute Force - Time = O(m*n), space = O(1)
+        # m, n = len(matrix), len(matrix[0])
+        # for i in range(m):
+        #     for j in range(n):
+        #         if matrix[i][j] == target:
+        #             return True
+        # return False
+
+        # Binary Search
+        def bin_search(arr):
+            lo, hi = 0, len(arr)
+            while lo <= hi:
+                mid = (lo + hi) // 2
+                if arr[mid] == target:
+                    return mid
+                elif arr[mid] < target:
+                    lo = mid + 1
+                else:
+                    hi = mid - 1
+            return -1
+        
+        m = len(matrix)
+        target_row = -1
         for i in range(m):
-            for j in range(n):
-                if matrix[i][j] == target:
-                    return True
-        return False
+            if matrix[i][0] <= target and matrix[i][-1] >= target:
+                target_row = i
+                break
+        
+        if target_row == -1:
+            return False
+        
+        target_col = bin_search(matrix[target_row])
+        return True if target_col != -1 else False
