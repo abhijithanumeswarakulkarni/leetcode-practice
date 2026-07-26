b058f90e143cde867e4d45391cbd7b746e7fc60a
Abhijit Hanumeswara Kulkarni <hanumesw@usc.edu>
2026-07-25 19:47:03 -0500

    Time: 0 ms (100%), Space: 19.5 MB (77.26%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -0,0 +1,9 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # Brute Force
+        m, n = len(matrix), len(matrix[0])
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == target:
+                    return True
+        return False
