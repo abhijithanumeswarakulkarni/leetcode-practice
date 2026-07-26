6a61958b0a6fbabeba7b146f673ea070b7f66047
Abhijit Hanumeswara Kulkarni <hanumesw@usc.edu>
2026-07-26 11:15:50 -0500

    Time: 7 ms (40.48%), Space: 19.4 MB (38.42%) - LeetHub

--- 2788-split-strings-by-separator/2788-split-strings-by-separator.py
+++ 2788-split-strings-by-separator/2788-split-strings-by-separator.py
@@ -1,6 +1,6 @@
 class Solution:
     def splitWordsBySeparator(self, words: List[str], separator: str) -> List[str]:
-        # Brute Force
+        # Brute Force - Time = O(n*k), space = O(1) - k = # of maximum splits of a word
         res = []
         for word in words:
             word_split = word.split(separator)
