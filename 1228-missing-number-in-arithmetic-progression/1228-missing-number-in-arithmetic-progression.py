ffc1d18e06af0f92d535f36dc671b419dfd9d923
Abhijit Hanumeswara Kulkarni <hanumesw@usc.edu>
2026-07-25 21:43:16 -0500

    Time: 2 ms (15.12%), Space: 19.3 MB (80.23%) - LeetHub

---
+++ 1228-missing-number-in-arithmetic-progression/1228-missing-number-in-arithmetic-progression.py
@@ -0,0 +1,15 @@
+class Solution:
+    def missingNumber(self, arr: List[int]) -> int:
+        # Brute force
+        # Edge case
+        if len(set(arr)) == 1:
+            return arr[0]
+        
+        diff = arr[1] - arr[0]
+        n = len(arr)
+        res = None
+        for i in range(2, n):
+            if arr[i] - arr[i-1] != diff:
+                res = arr[i] - diff
+                break
+        return res
