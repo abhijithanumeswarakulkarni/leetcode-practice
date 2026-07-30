71601749015bb320631d9e4e183327f679d1bd5c
Abhijit Hanumeswara Kulkarni <hanumesw@usc.edu>
2026-07-30 09:42:20 -0500

    Time: 95 ms (5.07%), Space: 28.6 MB (74.86%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # # Brute Force - Time = O(n^2), space = O(1) - TLE
+        # n = len(prices)
+        # maxi = 0
+        # for i in range(n-1):
+        #     for j in range(i+1, n):
+        #         maxi = max(maxi, prices[j] - prices[i])
+        # return maxi
+
+        # Sliding window
+        n = len(prices)
+        # Edge case
+        if n == 1:
+            return 0
+        l, r = 0, 1
+        maxi = prices[1] - prices[0] if prices[1] > prices[0] else 0
+        while r < n:
+            if l < r and prices[r] <= prices[l]:
+                l += 1
+            else:
+                maxi = max(maxi, prices[r] - prices[l])
+                r += 1
+        return maxi
