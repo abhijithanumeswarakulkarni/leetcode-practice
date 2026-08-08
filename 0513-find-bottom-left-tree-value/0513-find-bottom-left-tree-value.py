1de3ab23fc3ea3a69e237e6daf87aad0a3d52556
Abhijit Hanumeswara Kulkarni <hanumesw@usc.edu>
2026-08-08 11:31:20 -0500

    Time: 3 ms (49.36%), Space: 23 MB (16.86%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.py
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.py
@@ -5,20 +5,36 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    maxLevel = 0
     def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
-        levels = []
-        def bfs(node, lvl):
+        # levels = []
+        # def bfs(node, lvl):
+        #     if not node:
+        #         return
+            
+        #     if lvl >= len(levels):
+        #         levels.append([node.val])
+        #     else:
+        #         levels[lvl].append(node.val)
+            
+        #     bfs(node.left, lvl+1)
+        #     bfs(node.right, lvl+1)
+
+        # bfs(root, 0)
+
+        # return levels[-1][0]
+
+        self.res = root.val
+        def dfs(node, lvl):
             if not node:
                 return
+                
+            if lvl > self.maxLevel:
+                self.maxLevel = max(self.maxLevel, lvl)
+                self.res = node.val
             
-            if lvl >= len(levels):
-                levels.append([node.val])
-            else:
-                levels[lvl].append(node.val)
-            
-            bfs(node.left, lvl+1)
-            bfs(node.right, lvl+1)
-
-        bfs(root, 0)
+            dfs(node.left, lvl+1)
+            dfs(node.right, lvl+1)
         
-        return levels[-1][0]
+        dfs(root, 0)
+        return self.res
