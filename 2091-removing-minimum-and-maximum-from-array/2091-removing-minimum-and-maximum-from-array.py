915ef1fd6c544daf1c34f4adde9f40685f242853
Abhijit Hanumeswara Kulkarni <hanumesw@usc.edu>
2026-08-30 12:59:46 -0500

    Time: 12 ms (88.52%), Space: 33.7 MB (35.35%) - LeetHub

---
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.py
@@ -0,0 +1,35 @@
+class Solution:
+    def minimumDeletions(self, nums: List[int]) -> int:
+        # # Recursion + DP - TLE
+        # mini = min(nums)
+        # maxi = max(nums)
+        # def check(arr):
+        #     return (mini not in arr) and (maxi not in arr)
+        
+        # def solve(i, j, dp):
+        #     if i > j:
+        #         return float('inf')
+            
+        #     if check(nums[i:j]):
+        #         return 0
+        #     if dp[i][j] == -1:
+        #         dp[i][j] = min(1 + solve(i+1, j, dp), 1 + solve(i, j-1, dp))
+            
+        #     return dp[i][j]
+        
+        # n = len(nums)
+        # dp = [[-1] * (n+1) for _ in range(n+1)]
+        # return solve(0, n, dp)
+
+        mini = min(nums)
+        maxi = max(nums)
+        i, j = nums.index(mini), nums.index(maxi)
+        n = len(nums)
+
+        res = 1
+        if i < j:
+            res = min((j+1), (n-i), ((i+1) + (n-j)))
+        elif i > j:
+            res = min((i+1), (n-j), ((j+1) + (n-i)))
+        
+        return res
