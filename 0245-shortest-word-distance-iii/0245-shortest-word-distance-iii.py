eb834a44fa23b08e302c68f09194e2815ecf9573
Abhijit Hanumeswara Kulkarni <hanumesw@usc.edu>
2026-08-31 16:07:22 -0500

    Time: 43 ms (38.42%), Space: 35.3 MB (88.31%) - LeetHub

---
+++ 0245-shortest-word-distance-iii/0245-shortest-word-distance-iii.py
@@ -0,0 +1,35 @@
+class Solution:
+    def shortestWordDistance(self, wordsDict: List[str], word1: str, word2: str) -> int:
+        # # HMap - Optimised
+        # words = {}
+        # for idx, word in enumerate(wordsDict):
+        #     if word == word1 or word == word2:
+        #         if word in words:
+        #             words[word].append(idx)
+        #         else:
+        #             words[word] = [idx]
+        
+        # mini = float('inf')
+        # for x in words[word1]:
+        #     for y in words[word2]:
+        #         distance = abs(x-y)
+        #         if (distance > 0):
+        #             mini = min(mini, distance)
+        # return mini
+
+        # Like 2 pointers
+        first, second = None, None
+        mini = float('inf')
+        for index, word in enumerate(wordsDict):
+            if word == word1:
+                if second != None and index != second:
+                    mini = min(mini, abs(index - second))
+                first = index
+            if word == word2:
+                print(first, index, second)
+                if first != None and index != first:
+                    mini = min(mini, abs(index - first))
+                second = index
+        
+        return mini
+
