6d33a81203e97fea2df4cd6dd1827591f3003788
Abhijit Hanumeswara Kulkarni <hanumesw@usc.edu>
2026-09-01 20:24:46 -0500

    Time: 38 ms (90.59%), Space: 19.4 MB (18.58%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -4,29 +4,21 @@ class Solution:
         i, j = 0, 0
         res = ""
         turn = True
-
+        while i < m and j < n:
+            if turn:
+                res += word1[i]
+                i += 1
+            else:
+                res += word2[j]
+                j += 1
+            turn = not turn
         if m <= n:
-            while i < m:
-                if turn:
-                    res += word1[i]
-                    i += 1
-                else:
-                    res += word2[j]
-                    j += 1
-                turn = not turn
             while j < n:
                 res += word2[j]
                 j += 1
         else:
-            while j < n:
-                if turn:
-                    res += word1[i]
-                    i += 1
-                else:
-                    res += word2[j]
-                    j += 1
-                turn = not turn
             while i < m:
                 res += word1[i]
                 i += 1
+
         return res
