c1ffc9df647de58b1385db5b69f22bcd5d2fefbf
Abhijit Hanumeswara Kulkarni <hanumesw@usc.edu>
2026-09-02 20:20:23 -0500

    Time: 26 ms (75.61%), Space: 35.2 MB (85.98%) - LeetHub

---
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.py
@@ -0,0 +1,49 @@
+class Solution:
+    def uniformArray(self, nums1: list[int]) -> bool:
+        # # Brute force - TLE
+        # n = len(nums1)
+
+        # # Odd loop
+        # isOddPossible = True
+        # for i in range(n):
+        #     if nums1[i] % 2 == 0:
+        #         canHappen = False
+        #         for j in range(n):
+        #             diff = nums1[i] - nums1[j]
+        #             if j != i and diff >= 1 and (diff) % 2 != 0:
+        #                 canHappen = True
+        #                 break
+        #         if not canHappen:
+        #             isOddPossible = False
+        #             break
+        # if isOddPossible:
+        #     return True
+        
+        # # Even loop
+        # isEvenPossible = True
+        # for i in range(n):
+        #     if nums1[i] % 2 != 0:
+        #         canHappen = False
+        #         for j in range(n):
+        #             diff = nums1[i] - nums1[j]
+        #             if j != i and diff >= 1 and (diff) % 2 == 0:
+        #                 canHappen = True
+        #                 break
+        #         if not canHappen:
+        #             isEvenPossible = False
+        #             break
+        # if isEvenPossible:
+        #     return True
+        
+        # return False
+
+        mn = nums1[0]
+        hasOdd = False
+        for v in nums1:
+            if v < mn:
+                mn = v
+            if v & 1:
+                hasOdd = True
+        if mn & 1:
+            return True
+        return not hasOdd
