686d0ef20e4116bfb12f82e38d7c75a3f44a0348
Abhijit Hanumeswara Kulkarni <hanumesw@usc.edu>
2026-09-04 12:31:24 -0500

    Time: 1 ms (72.45%), Space: 19.4 MB (48.02%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,20 +1,32 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        n = len(cost)
-        def solve(index, dp):
-            if index >= n:
-                return 0
+        # n = len(cost)
+        # def solve(index, dp):
+        #     if index >= n:
+        #         return 0
             
-            if dp[index] == -1:
-                opt1 = cost[index] + solve(index+1, dp)
-                opt2 = cost[index] + solve(index+2, dp)
+        #     if dp[index] == -1:
+        #         opt1 = cost[index] + solve(index+1, dp)
+        #         opt2 = cost[index] + solve(index+2, dp)
 
-                dp[index] = min(opt1, opt2)
+        #         dp[index] = min(opt1, opt2)
             
-            return dp[index]
+        #     return dp[index]
         
+        # dp = [-1] * (n+2)
+        # startZero = solve(0, dp)
+        # startOne = solve(1, dp)
+        
+        # return min(startZero, startOne)
+
+        n = len(cost)
         dp = [-1] * (n+2)
-        startZero = solve(0, dp)
-        startOne = solve(1, dp)
+        dp[n] = 0
+        dp[n+1] = 0
+
+        for index in range(n-1, -1, -1):
+            opt1 = cost[index] + dp[index+1]
+            opt2 = cost[index] + dp[index+2]
+            dp[index] = min(opt1, opt2)
         
-        return min(startZero, startOne)
+        return min(dp[0], dp[1])
