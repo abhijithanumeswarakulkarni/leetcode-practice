4dee453e1c791d4843254be653d46f97f77aefe0
Abhijit Hanumeswara Kulkarni <hanumesw@usc.edu>
2026-09-04 12:39:57 -0500

    Time: 4 ms (2.04%), Space: 19.2 MB (90.7%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,21 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        def solve(index, canRob, dp):
+            if index == n:
+                return 0
+            
+            if dp[index][canRob] == -1:
+                opt1, opt2 = 0, 0
+                if canRob:
+                    opt1 = nums[index] + solve(index + 1, 0, dp)
+                else:
+                    opt2 = solve(index + 1, 1, dp)
+                opt3 = solve(index + 1, canRob, dp)
+
+                dp[index][canRob] = max(opt1, opt2, opt3)
+            
+            return dp[index][canRob]
+        
+        dp = [[-1] * 2 for _ in range(n+1)]
+        return solve(0, 1, dp)
