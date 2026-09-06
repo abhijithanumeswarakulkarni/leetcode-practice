04ce9d5d9ce156f6d645edc4f1a380c50091eb75
Abhijit Hanumeswara Kulkarni <hanumesw@usc.edu>
2026-09-06 09:25:11 -0500

    Time: 347 ms (66.43%), Space: 75.6 MB (48.64%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -1,24 +1,42 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
-        m, n = len(s), len(t)
+        # m, n = len(s), len(t)
 
-        def solve(i, j, dp):
-            if j == n:
-                return 1
+        # def solve(i, j, dp):
+        #     if j == n:
+        #         return 1
                 
-            if i == m:
-                return 0
+        #     if i == m:
+        #         return 0
             
-            if dp[i][j] == -1:
+        #     if dp[i][j] == -1:
+        #         opt1 = 0
+        #         if s[i] == t[j]:
+        #             opt1 = solve(i+1, j+1, dp)
+                
+        #         opt2 = solve(i+1, j, dp)
+
+        #         dp[i][j] = opt1 + opt2
+            
+        #     return dp[i][j]
+        
+        # dp = [[-1] * n for _ in range(m)]
+        # return solve(0, 0, dp)
+
+        m, n = len(s), len(t)
+        dp = [[-1] * (n+1) for _ in range(m+1)]
+        dp[m][n] = 1
+        for i in range(m):
+            dp[i][n] = 1
+        for j in range(n):
+            dp[m][j] = 0
+        
+        for i in range(m-1, -1, -1):
+            for j in range(n-1, -1, -1):
                 opt1 = 0
                 if s[i] == t[j]:
-                    opt1 = solve(i+1, j+1, dp)
-                
-                opt2 = solve(i+1, j, dp)
-
+                    opt1 = dp[i+1][j+1]
+                opt2 = dp[i+1][j]
                 dp[i][j] = opt1 + opt2
-            
-            return dp[i][j]
         
-        dp = [[-1] * n for _ in range(m)]
-        return solve(0, 0, dp)
+        return dp[0][0]
