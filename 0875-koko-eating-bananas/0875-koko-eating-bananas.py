a854a5d5d5b6b1abaa52e38ef7e616acf1741ffb
Abhijit Hanumeswara Kulkarni <hanumesw@usc.edu>
2026-09-06 09:51:42 -0500

    Time: 260 ms (5.05%), Space: 20.6 MB (44.42%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,25 +1,23 @@
-import math
-
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        # # Brute Force = Time = O(n*k), space = O(1) -> k = max element in piles - TLE
-        # for k in range(1, max(piles)+1):
-        #     count = 0
-        #     for pile in piles:
-        #         count += math.ceil(pile / k)
-        #     if count <= h:
-        #         return k
-        # return -1 # If koko can't finish at all, ideally shouldn't reach this ever
+        n = len(piles)
+        piles.sort()
+        lo, hi = 1, piles[-1]
+        mini = -1
 
-        # Binary Search - Time = O(n*log(k)), space = O(1)
-        left, right = 1, max(piles)
-        while left < right:
-            mid = (left + right) // 2
-            hrs_spent = 0
-            for pile in piles:
-                hrs_spent += math.ceil(pile / mid)
-            if hrs_spent <= h:
-                right = mid
+        while lo <= hi:
+            mid = (lo + hi) // 2
+            hLeft = h
+            i = 0
+            while i < n:
+                hLeft -= math.ceil(piles[i] / mid)
+                if hLeft < 0:
+                    break
+                i += 1
+            if i == n:
+                mini = mid
+                hi = mid-1
             else:
-                left = mid+1
-        return right
+                lo = mid + 1
+        
+        return mini
