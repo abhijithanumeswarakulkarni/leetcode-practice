b66b101580fdfeb297280fbf9cfc5da27315d282
Abhijit Hanumeswara Kulkarni <hanumesw@usc.edu>
2026-09-05 19:33:48 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,6 +1,6 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        # # Brute Force - Time = O(n^2), space = O(1) - TLE
+        # # Brute force - Obv TLE
         # n = len(temperatures)
         # res = [0] * n
         # for i in range(n-1):
@@ -8,19 +8,22 @@ class Solution:
         #         if temperatures[j] > temperatures[i]:
         #             res[i] = (j-i)
         #             break
+        
         # return res
 
         # Stack
+        stack = [temperatures[-1]]
         n = len(temperatures)
-        stack = [(temperatures[n-1], n-1)]
         res = [0] * n
         for i in range(n-2, -1, -1):
-            if stack[-1][0] > temperatures[i]:
-                res[i] = stack[-1][1] - i
-            else:
-                while stack and stack[-1][0] <= temperatures[i]:
-                    stack.pop()
+            if temperatures[i] > stack[-1]:
+                poppedElements = []
+                while stack and temperatures[i] > stack[-1]:
+                    poppedElements.append(stack.pop())
                 if stack:
-                    res[i] = stack[-1][1] - i
-            stack.append((temperatures[i], i))
+                    res[i] = len(poppedElements) + 1
+                stack += poppedElements[::-1]
+            else:
+                res[i] = 1
+            stack.append(temperatures[i])
         return res
