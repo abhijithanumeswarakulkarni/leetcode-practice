0853714f6ba545fe33fd37bb7b1744d37f7ab769
Abhijit Hanumeswara Kulkarni <hanumesw@usc.edu>
2026-09-06 19:58:54 -0500

    Time: 51 ms (5.1%), Space: 30 MB (5.04%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,22 +1,19 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
-        def solve(index, canBuy, dp):
+        def solve(index, canSell, dp):
             if index == n:
                 return 0
             
-            if dp[index][canBuy] == -1:
+            if dp[index][canSell] == -1:
                 opt1 = 0
-                if canBuy:
-                    opt1 = solve(index+1, 0, dp) - prices[index]
+                if canSell:
+                    opt1 = prices[index] + solve(index+1, 0, dp)
                 
-                opt2 = 0
-                if not canBuy:
-                    opt2 = prices[index] + solve(index+1, 1, dp)
-                
-                opt3 = solve(index+1, canBuy, dp)
-                dp[index][canBuy] = max(opt1, opt2, opt3)
-            return dp[index][canBuy]
+                opt2 = solve(index+1, 1, dp) - prices[index]
+                opt3 = solve(index+1, canSell, dp)
+                dp[index][canSell] = max(opt1, opt2, opt3)
+            return dp[index][canSell]
         
         dp = [[-1] * 2 for _ in range(n)]
-        return solve(0, 1, dp)
+        return solve(0, 0, dp)
