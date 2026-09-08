5c776a08519863c85501faa8f47fd712c58479d3
Abhijit Hanumeswara Kulkarni <hanumesw@usc.edu>
2026-09-07 19:47:27 -0500

    Time: 55 ms (72.9%), Space: 29.7 MB (38.42%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,23 +1,13 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        # # Brute Force - Time = O(n^2), space = O(n) - TLE
-        # max_area = float('-inf')
-        # n = len(height)
-        # for left in range(n-1):
-        #     for right in range(left+1, n):
-        #         curr_area = (right-left) * min(height[left], height[right])
-        #         max_area = max(max_area, curr_area)
-        # return max_area
-
-        # 2 Pointers - Time = O(n), space = O(1)
-        left, right = 0, len(height)-1
-        max_area = float('-inf')
-        while left < right:
-            curr_area = (right-left) * min(height[left], height[right])
-            max_area = max(max_area, curr_area)
-            if height[left] < height[right]:
-                left += 1
+        maxi = float('-inf')
+        n = len(height)
+        i, j = 0, n-1
+        while i < j:
+            currArea = (j-i) * min(height[i], height[j])
+            maxi = max(maxi, currArea)
+            if height[i] < height[j]:
+                i += 1
             else:
-                right -= 1
-        return max_area
-        
+                j -= 1
+        return maxi
