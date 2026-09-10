b6179e2a839a2bd9cfc27f294079337259c02673
Abhijit Hanumeswara Kulkarni <hanumesw@usc.edu>
2026-09-10 11:23:30 -0500

    Time: 3 ms (59.95%), Space: 19.5 MB (6.31%) - LeetHub

---
+++ 0819-most-common-word/0819-most-common-word.py
@@ -0,0 +1,34 @@
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        paragraphSplit = map(lambda x: x.lower(), paragraph.split(" "))
+        frq = {}
+        for word in paragraphSplit:
+            if word.isalpha():
+                if word in frq:
+                    frq[word] += 1
+                else:
+                    frq[word] = 1
+            else:
+                key = ""
+                for letter in word:
+                    if letter.isalpha():
+                        key += letter
+                    elif key:
+                        if key in frq:
+                            frq[key] += 1
+                        else:
+                            frq[key] = 1
+                        key = ""
+                if key:
+                    if key in frq:
+                        frq[key] += 1
+                    else:
+                        frq[key] = 1
+        
+        print(frq)
+        sortedFrq = list(sorted(frq.items(), key=lambda x: -x[1]))
+        for item in sortedFrq:
+            if item[0] not in banned:
+                return item[0]
+        
+        return "" # Every word in paragraph is banned
